67871194d48b2431ba8119d04defa9d8476f3ce6
ZhangForFaith <57511700+ZhangForFaith@users.noreply.github.com>
2022-11-14 16:18:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小孩',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ozmwq6yfxS4lhX2_iavG2TyEcskQ',
+      id: 'ozmwq6_xVw6nEUjoko7xyRrAYpNE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'rz8RN-xdmkcBqrl27NasZk_YofZgtktj0sPRMzZ1isE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
