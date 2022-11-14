c9bafd137e39e544fe3be243ee9b268abd3fd960
ZhangForFaith <57511700+ZhangForFaith@users.noreply.github.com>
2022-11-14 16:10:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ozmwq6_xVw6nEUjoko7xyRrAYpNE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'rz8RN-xdmkcBqrl27NasZk_YofZgtktj0sPRMzZ1isE	',
+      useTemplateId: 'rz8RN-xdmkcBqrl27NasZk_YofZgtktj0sPRMzZ1isE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-22',
       birthday_message:'1999-10-22',
