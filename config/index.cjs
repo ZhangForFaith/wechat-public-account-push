579c37e58ca126e8e75a163f0cadad879c770fc6
ZhangForFaith <57511700+ZhangForFaith@users.noreply.github.com>
2022-11-14 15:40:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ozmwq6_xVw6nEUjoko7xyRrAYpNE',
+      id: 'ozmwq6yfxS4lhX2_iavG2TyEcskQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'hinWmIZbO6tIe55xhdFjEwSe3KO93rfrOpHgcMPwVUs',
+      useTemplateId: 'BDX9PZSo5YFkGAtlvyPKCHZTsfJ_wf9Bp6go7lqnCeI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-22',
       festivals: [
@@ -42,9 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-10-30' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-10-30' }
       ],
     },
   ],
